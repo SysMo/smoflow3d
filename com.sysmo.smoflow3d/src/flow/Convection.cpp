629c4b2c56ecde75f2f3584ddd5c4f16f3e87692
Atanas Pavlov <atanas.pavlov@sysmoltd.com>
2013-08-11 10:35:00 +0300

    Created FreeConvection and ForcedConvection classes.
    Signed-off-by: Atanas Pavlov <atanas.pavlov@sysmoltd.com>

---
+++ com.sysmo.smoflow3d/src/flow/Convection.cpp
@@ -0,0 +1,32 @@
+/*
+ * Convection.cpp
+ *
+ *  Created on: Aug 10, 2013
+ *      Author: Atanas Pavlov
+ *	 Copyright: SysMo Ltd., Bulgaria
+ */
+
+#include "Convection.h"
+
+Convection::Convection() {
+
+}
+
+Convection::~Convection() {
+}
+
+void Convection::init(MediumState* fluidState, ThermalNode* wallNode) {
+	this->fluidState = fluidState;
+	this->wallNode = wallNode;
+	this->filmState = MediumState_new(fluidState->getMedium());
+	MediumState_register(this->filmState);
+}
+
+void Convection::getFlow_Wall(HeatFlow* flow) {
+	flow->enthalpyFlowRate = - heatFlowRate;
+}
+
+void Convection::getFlow_Fluid(FluidFlow* flow) {
+	flow->enthalpyFlowRate = heatFlowRate;
+	flow->massFlowRate = 0;
+}
