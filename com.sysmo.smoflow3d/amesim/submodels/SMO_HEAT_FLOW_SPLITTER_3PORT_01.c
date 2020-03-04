523d30356ee93522e7606d49c04382b8b26747c9
milen.borisov <milen.borisov@SYSMO>
2020-03-04 10:16:37 +0200

    Implemented SMO_HEAT_FLOW_SPLITTER_3PORT_01
    Signed-off-by: milen.borisov <milen.borisov@SYSMO>

---
+++ com.sysmo.smoflow3d/amesim/submodels/SMO_HEAT_FLOW_SPLITTER_3PORT_01.c
@@ -0,0 +1,170 @@
+/* Submodel SMO_HEAT_FLOW_SPLITTER_3PORT_01 skeleton created by AME Submodel editing utility
+   ?? ??? 4 10:13:00 2020 */
+
+
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ameutils.h"
+/* *******************************************************************************
+TITLE :
+
+------------------------------------------------------------------------------
+DESCRIPTION :
+
+------------------------------------------------------------------------------
+USAGE :
+
+------------------------------------------------------------------------------
+PARAMETER SETTINGS :
+
+------------------------------------------------------------------------------
+REVISIONS :
+
+******************************************************************************* */
+
+#define _SUBMODELNAME_ "SMO_HEAT_FLOW_SPLITTER_3PORT_01"
+
+/* >>>>>>>>>>>>Insert Private Code Here. */
+#include "flow/FlowBase.h"
+
+#define _heatFlowIndexOutlet ic[0]
+#define _heatFlowOutlet ps[0]
+/* <<<<<<<<<<<<End of Private Code. */
+void smo_heat_flow_splitter_3port_01in_(int *n, int ic[1], void *ps[1])
+
+{
+   int loop, error;
+/* >>>>>>>>>>>>Extra Initialization Function Declarations Here. */
+/* <<<<<<<<<<<<End of Extra Initialization declarations. */
+   loop = 0;
+   error = 0;
+
+
+/* >>>>>>>>>>>>Initialization Function Check Statements. */
+/* <<<<<<<<<<<<End of Initialization Check Statements. */
+
+   if(error == 1)
+   {
+      amefprintf(stderr, "\nWarning in %s instance %d.\n", _SUBMODELNAME_, *n);
+   }
+   else if(error == 2)
+   {
+      amefprintf(stderr, "\nFatal error in %s instance %d.\n", _SUBMODELNAME_, *n);
+      amefprintf(stderr, "Terminating the program.\n");
+      AmeExit(1);
+   }
+
+
+/* >>>>>>>>>>>>Initialization Function Executable Statements. */
+   _heatFlowOutlet = HeatFlow_new();
+   _heatFlowIndexOutlet = HeatFlow_register(_heatFlowOutlet);
+/* <<<<<<<<<<<<End of Initialization Executable Statements. */
+}
+
+/*  There are 3 ports.
+
+   Port 1 has 2 variables:
+
+      1 thermalNodeIndexDup1     duplicate of thermalNodeIndex
+      2 heatFlowIndex1           heat flow index at port 1 [smoHFL] basic variable input  UNPLOTTABLE
+
+   Port 2 has 2 variables:
+
+      1 heatFlowIndexOut     heat flow index out [smoHFL] multi line macro 'smo_heat_flow_splitter_3port_01_macro0_'  UNPLOTTABLE
+      2 thermalNodeIndex     thermal node index  [smoTHN] basic variable input  UNPLOTTABLE
+
+   Port 3 has 2 variables:
+
+      1 thermalNodeIndexDup3     duplicate of thermalNodeIndex
+      2 heatFlowIndex3           heat flow index at port 3 [smoHFL] basic variable input  UNPLOTTABLE
+*/
+
+/*  There are 0 internal variables.
+
+*/
+
+#if 0
+
+/* THE CALCULATION FUNCTION WILL NOT BE CALLED. */
+
+void smo_heat_flow_splitter_3port_01_(int *n, double *heatFlowIndex1
+      , double *heatFlowIndexOut, double *thermalNodeIndex
+      , double *heatFlowIndex3, int ic[1], void *ps[1])
+
+{
+   int loop;
+/* >>>>>>>>>>>>Extra Calculation Function Declarations Here. */
+/* <<<<<<<<<<<<End of Extra Calculation declarations. */
+   loop = 0;
+
+/* Common -> SI units conversions. */
+
+/*   *heatFlowIndex1 *= ??; CONVERSION UNKNOWN [smoHFL] */
+/*   *heatFlowIndexOut *= ??; CONVERSION UNKNOWN [smoHFL] */
+/*   *thermalNodeIndex *= ??; CONVERSION UNKNOWN [smoTHN] */
+/*   *heatFlowIndex3 *= ??; CONVERSION UNKNOWN [smoHFL] */
+
+/*
+   Set all submodel outputs below:
+
+*/
+
+
+
+/* >>>>>>>>>>>>Calculation Function Executable Statements. */
+/* <<<<<<<<<<<<End of Calculation Executable Statements. */
+
+/* SI -> Common units conversions. */
+
+/*   *heatFlowIndex1 /= ??; CONVERSION UNKNOWN [smoHFL] */
+/*   *heatFlowIndexOut /= ??; CONVERSION UNKNOWN [smoHFL] */
+/*   *thermalNodeIndex /= ??; CONVERSION UNKNOWN [smoTHN] */
+/*   *heatFlowIndex3 /= ??; CONVERSION UNKNOWN [smoHFL] */
+}
+
+#endif
+extern double smo_heat_flow_splitter_3port_01_macro0_(int *n
+      , double *heatFlowIndex1, double *heatFlowIndex3, int ic[1]
+      , void *ps[1])
+
+{
+   double heatFlowIndexOut;
+   int loop;
+/* >>>>>>>>>>>>Extra Macro Function macro0 Declarations Here. */
+/* <<<<<<<<<<<<End of Extra Macro macro0 declarations. */
+   loop = 0;
+
+/* Common -> SI units conversions. */
+
+/*   *heatFlowIndex1 *= ??; CONVERSION UNKNOWN [smoHFL] */
+/*   *heatFlowIndex3 *= ??; CONVERSION UNKNOWN [smoHFL] */
+
+/*
+   Define and return the following macro variable:
+
+   heatFlowIndexOut = ??;
+*/
+
+
+/* >>>>>>>>>>>>Macro Function macro0 Executable Statements. */
+   HeatFlow* flowIn1 = HeatFlow_get(*heatFlowIndex1);
+   HeatFlow* flowIn3 = HeatFlow_get(*heatFlowIndex3);
+
+   double enthalpyFlowIn = HeatFlow_getEnthalpyFlowRate(flowIn1) + HeatFlow_getEnthalpyFlowRate(flowIn3);
+   HeatFlow_setEnthalpyFlowRate(_heatFlowOutlet, enthalpyFlowIn);
+
+   heatFlowIndexOut = _heatFlowIndexOutlet;
+/* <<<<<<<<<<<<End of Macro macro0 Executable Statements. */
+
+/* SI -> Common units conversions. */
+
+/*   *heatFlowIndex1 /= ??; CONVERSION UNKNOWN [smoHFL] */
+/*   *heatFlowIndex3 /= ??; CONVERSION UNKNOWN [smoHFL] */
+
+/*   *heatFlowIndexOut /= ??; CONVERSION UNKNOWN [smoHFL] */
+
+   return heatFlowIndexOut;
+}
+
